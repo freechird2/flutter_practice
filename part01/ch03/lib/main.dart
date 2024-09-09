5bc8af80014e5a435077b8bd263b49794407787f
Suhyeon Jeon <suhyeon.jeon@fig.xyz>
2024-09-09 09:39:11 +0900

    part01_ch03

---
+++ part01/ch03/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Body(),
+  ));
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('asdfasdf'),
+        ),
+        body: Column(
+          children: [ExampleStateless(), ExampleStateful(index: 3)],
+        ),
+      ),
+    );
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(flex: 1, child: Container(color: Colors.red));
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  final int index;
+  const ExampleStateful({required this.index, super.key});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  late int _index;
+  late TextEditingController textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.index;
+    textController = TextEditingController();
+  }
+
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (_index == 5) {
+              _index = 0;
+              return;
+            }
+
+            _index++;
+          });
+        },
+        child: Container(
+          width: double.infinity,
+          color: Colors.blue.withOpacity(_index/5),
+          child: Center(child: Text('$_index')),
+        ),
+      ),
+    );
+  }
+}
