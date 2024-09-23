8edf8f9cc55010a7cf35e3aab029c68f21e5a54f
Suhyeon Jeon <suhyeon.jeon@fig.xyz>
2024-09-23 09:43:11 +0900

    part01_ch01

---
+++ part01/ch06/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter에서 외부 라이브러리 사용하기'),
+        ),
+        body: Body(),
+      ),
+    )
+  ));
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RandomWords();
+  }
+}
+
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final wordList = generateWordPairs().take(5).toList();
+    final widgets = wordList.map((word) => Text(word.asString, style: TextStyle(fontSize: 32))).toList();
+
+    return Column(
+      children: widgets,
+    );
+  }
+}
